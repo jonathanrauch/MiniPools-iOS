3cc03af0d2e6ded341e7ee60fd56f91667beeded
Sebi Kaplun <skapln@paypal.com>
2017-12-20 11:46:43 +0200

    mini-pools: mock all pools tableview

---
+++ mini-pools-ios/AllPoolsTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  mini-pools-ios
+//
+//  Created by Kaplún, Sebi on 12/20/17.
+//  Copyright © 2017 Kaplún, Sebi. All rights reserved.
+//
+
+import UIKit
+
+class AllPoolsTableViewController: UITableViewController {
+
+    @IBOutlet weak var poolsTableView: UITableView!
+    
+    let pools_hardcoded = ["pool1", "pool2", "pool3", "pool4", "pool5"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pools_hardcoded.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AllPoolsCell", for: indexPath)
+        
+        cell.textLabel?.text = pools_hardcoded[indexPath.row]
+        
+        return cell
+    }
+
+}
+

78b96445573e861dff5e62d5bd063cbc9d4ab74e
Sebi Kaplun <skapln@paypal.com>
2017-12-20 15:52:20 +0200

    mini-pools: add network call

--- mini-pools-ios/Workers/PoolsWorker.swift
+++ mini-pools-ios/Workers/PoolsWorker.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2017 Kaplún, Sebi. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+import SwiftyJSON
+
+class PoolsWorker {
+    
+    static let BASE_SERVER = "https://mini-pools.herokuapp.com"
+    static let FETCH_POOLS_URL = "/pools"
+
+    static func fetchPools(completion: @escaping ([PoolModel]) -> Void) {
+        Alamofire.request("\(BASE_SERVER)\(FETCH_POOLS_URL)").responseJSON { response in
+            var pools : [PoolModel] = []
+            if let responseResult = response.result.value {
+                let json = JSON(responseResult)
+                for poolJson in json.arrayValue {
+                    let pool = PoolModel(dictionary: poolJson)
+                    pools.append(pool)
+                }
+            }
+            completion(pools)
+        }
+    }
+}

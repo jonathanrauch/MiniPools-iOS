4585665734d50ed9d07e543ef145a589fb509560
Sebi Kaplun <skapln@paypal.com>
2017-12-20 11:24:38 +0200

    mini-pools: create base project

---
+++ mini-pools-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mini-pools-ios
+//
+//  Created by Kaplún, Sebi on 12/20/17.
+//  Copyright © 2017 Kaplún, Sebi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

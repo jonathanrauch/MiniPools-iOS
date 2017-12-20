73dcb0c012a90f9481fdbdd929f4fd89201b6373
Sebi Kaplun <skapln@paypal.com>
2017-12-20 17:05:18 +0200

    mini-pools: create vc for the pool details

---
+++ mini-pools-ios/Views/ContributionCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ContributionCellTableViewCell.swift
+//  mini-pools-ios
+//
+//  Created by Kaplún, Sebi on 12/20/17.
+//  Copyright © 2017 Kaplún, Sebi. All rights reserved.
+//
+
+import UIKit
+
+class ContributionCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contributorLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

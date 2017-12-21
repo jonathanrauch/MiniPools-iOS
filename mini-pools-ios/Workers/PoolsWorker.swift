5270aa85d0d8ea283e925397bb3c7e260f29140e
Sebi Kaplun <skapln@paypal.com>
2017-12-21 15:18:25 +0200

    mini-pools: add call to the server for contributions

--- mini-pools-ios/Workers/PoolsWorker.swift
+++ mini-pools-ios/Workers/PoolsWorker.swift
@@ -24,4 +24,15 @@ class PoolsWorker {
             completion(pools)
         }
     }
+    
+    static func contributeToPool(poolId: Int, amountValue: Int, amountCurrency: String, note: String?, contributor: PersonModel, completion: @escaping (ContributionModel) -> Void) {
+        let parameters: Parameters = [API.Parameters.amountValue: amountValue, API.Parameters.amountCurrency: amountCurrency, API.Parameters.note: note ?? "",  API.Parameters.contributorId: contributor.id]
+        Alamofire.request("\(API.baseServerUrl)\(API.allPools)/\(poolId)\(API.contributions)", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            if let responseResult = response.result.value {
+                let json = JSON(responseResult)
+                let contribution = ContributionModel(dictionary: json, contributor: contributor)
+                completion(contribution)
+            }
+        }
+    }
 }

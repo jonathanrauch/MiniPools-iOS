4bd0e6abd88188702cf9d75b771284fa08faa7bb
Shay Davidson <sdavidson@paypal.com>
2019-01-10 14:54:10 +0200

    added pods to the project

---
+++ mini-pools-ios/scaffold/Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  Extensions.swift
+//  mini-pools-ios
+//
+//  Created by Kaplún, Sebi on 12/20/17.
+//  Copyright © 2017 Kaplún, Sebi. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+  func shortDate() -> String {
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "MMM d, yyyy"
+    return dateformatter.string(from: self)
+  }
+}
+
+extension String {
+  func getSymbolForCurrencyCode() -> String {
+    let locale = NSLocale(localeIdentifier: self)
+    return locale.displayName(forKey: NSLocale.Key.currencySymbol, value: self) ?? self
+  }
+}
+
+class DateUtil {
+  static func initDate(string : String) -> Date? {
+    let dateFor: DateFormatter = DateFormatter()
+    dateFor.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    return dateFor.date(from: string)
+  }
+}
---
+++ mini-pools-ios/scaffold/Models.swift
@@ -0,0 +1,92 @@
+//
+//  Models.swift
+//  mini-pools-ios
+//
+//  Created by Davidson, Shay on 10/01/2019.
+//  Copyright © 2019 Kaplún, Sebi. All rights reserved.
+//
+
+import SwiftyJSON
+
+struct PoolModel {
+  var id: Int
+  var name: String
+  var goalAmountValue: Int
+  var goalAmountCurrency: String
+  var extra: String?
+  var createdAt: Date
+  var updatedAt: Date
+  var creator: PersonModel?
+  var contributions: [ContributionModel] = []
+  
+  init(dictionary: JSON) {
+    self.id = dictionary["id"].intValue
+    self.name = dictionary["name"].stringValue
+    self.goalAmountValue = dictionary["goalAmountValue"].intValue
+    self.goalAmountCurrency = dictionary["goalAmountCurrency"].stringValue
+    self.extra = dictionary["extra"].string
+    self.createdAt = DateUtil.initDate(string: dictionary["createdAt"].stringValue)!
+    self.updatedAt = DateUtil.initDate(string: dictionary["updatedAt"].stringValue)!
+    
+    if dictionary["creator"] != JSON.null {
+      self.creator = PersonModel(dictionary: dictionary["creator"])
+    }
+    
+    for contributionJson in dictionary["contributions"].arrayValue {
+      let contribution = ContributionModel(dictionary: contributionJson)
+      contributions.append(contribution)
+    }
+  }
+}
+
+struct PersonModel {
+  var id: Int
+  var name: String
+  var avatarUrl: String
+  var extra: String?
+  var createdAt: Date
+  var updatedAt: Date
+  
+  init(dictionary: JSON) {
+    self.id = dictionary["id"].intValue
+    self.name = dictionary["name"].stringValue
+    self.avatarUrl = dictionary["avatarUrl"].stringValue
+    self.extra = dictionary["extra"].string
+    self.createdAt = DateUtil.initDate(string: dictionary["createdAt"].stringValue)!
+    self.updatedAt = DateUtil.initDate(string: dictionary["updatedAt"].stringValue)!
+  }
+}
+
+struct ContributionModel {
+  var id: Int
+  var amountValue: Int
+  var amountCurrency: String
+  var note: String?
+  var extra: String?
+  var createdAt: Date
+  var updatedAt: Date
+  var contributor : PersonModel
+  
+  init(dictionary: JSON ) {
+    self.id = dictionary["id"].intValue
+    self.amountValue = dictionary["amountValue"].intValue
+    self.amountCurrency = dictionary["amountCurrency"].stringValue
+    self.note = dictionary["note"].string
+    self.extra = dictionary["extra"].string
+    self.createdAt = DateUtil.initDate(string: dictionary["createdAt"].stringValue)!
+    self.updatedAt = DateUtil.initDate(string: dictionary["updatedAt"].stringValue)!
+    self.contributor = PersonModel(dictionary:dictionary["contributor"])
+  }
+  
+  init(dictionary: JSON, contributor: PersonModel) {
+    self.id = dictionary["id"].intValue
+    self.amountValue = dictionary["amountValue"].intValue
+    self.amountCurrency = dictionary["amountCurrency"].stringValue
+    self.note = dictionary["note"].string
+    self.extra = dictionary["extra"].string
+    self.createdAt = DateUtil.initDate(string: dictionary["createdAt"].stringValue)!
+    self.updatedAt = DateUtil.initDate(string: dictionary["updatedAt"].stringValue)!
+    self.contributor = contributor
+  }
+}
+

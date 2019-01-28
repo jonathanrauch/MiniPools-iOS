981cfc2de373b7632297b17771dcddf08004c5c4
Shay Davidson <sdavidson@paypal.com>
2019-01-28 13:19:57 +0200

    added first vc

---
+++ mini-pools-ios/misc/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  mini-pools-ios
+//
+//  Created by Kaplún, Sebi on 12/20/17.
+//  Copyright © 2017 Kaplún, Sebi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  var navController: UINavigationController?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // setup navigation controller
+    let navController = UINavigationController()
+    navController.viewControllers = [HomeViewController()]
+    self.navController = navController
+    
+    // setup window
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = navController
+    window.makeKeyAndVisible()
+    self.window = window
+    
+    return true
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) {
+  }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) {
+  }
+  
+  func applicationWillEnterForeground(_ application: UIApplication) {
+  }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+  }
+  
+  func applicationWillTerminate(_ application: UIApplication) {
+  }
+}
---
+++ mini-pools-ios/scaffold/viewControllers/PoolFormViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PoolFormViewController.swift
+//  mini-pools-ios
+//
+//  Created by Davidson, Shay on 28/01/2019.
+//  Copyright © 2019 Kaplún, Sebi. All rights reserved.
+//
+
+import UIKit
+
+class PoolFormViewController: UIViewController {
+}
